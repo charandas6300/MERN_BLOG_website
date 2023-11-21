b8444c7f1a2aedf5f8d34c64e17f28fd6b3a4c44
charandas6300 <charandas.v20@iiits.in>
2023-11-21 21:20:42 +0530

    updating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
+import Post from './Post';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href = "#" className = "logo">MyBlog</a>
-        <nav>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
-        </nav>
-      </header>
-      <div className="entries">
-        <div className = "post">
-        <div className="image">
-          <img src="https://imgs.search.brave.com/-zhYr-__chwocwaLLbuMMDm87G6vbQlSDE0QTMgpxew/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDg2/Mzg4MDEwL3Bob3Rv/L2xhbWJvcmdoaW5p/LWF2ZW50YWRvci1s/cDcwMC00LXN1cGVy/Y2FyLW1vZGVsLWNh/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9TDExQkQ2LUhr/UTZUcGFaUkVhVjNf/akVTUThlWFAtZG5H/eWFQNWEzaXBEdz0" alt="img1"></img>
-          </div>
-          <div className="texts">
-            <h2>HURACÁN</h2>
-            <p>The upcoming Lamborghini Revuelto is the first in the Lambo range to be equipped with an electric drivetrain thanks to its plug-in hybrid system. It has a 6.5-litre unit with a peak torque output of 725Nm at 6,750rpm and 813bhp of maximum power at 9,250 rpm. </p>
-          </div>
-        </div>
-        <div className = "post">
-        <div className="image">
-          <img src="https://imgs.search.brave.com/-zhYr-__chwocwaLLbuMMDm87G6vbQlSDE0QTMgpxew/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDg2/Mzg4MDEwL3Bob3Rv/L2xhbWJvcmdoaW5p/LWF2ZW50YWRvci1s/cDcwMC00LXN1cGVy/Y2FyLW1vZGVsLWNh/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9TDExQkQ2LUhr/UTZUcGFaUkVhVjNf/akVTUThlWFAtZG5H/eWFQNWEzaXBEdz0" alt="img1"></img>
-          </div>
-          <div className="texts">
-            <h2>HURACÁN</h2>
-            <p>The upcoming Lamborghini Revuelto is the first in the Lambo range to be equipped with an electric drivetrain thanks to its plug-in hybrid system. It has a 6.5-litre unit with a peak torque output of 725Nm at 6,750rpm and 813bhp of maximum power at 9,250 rpm. </p>
-          </div>
-        </div>
-        <div className = "post">
-        <div className="image">
-          <img src="https://imgs.search.brave.com/-zhYr-__chwocwaLLbuMMDm87G6vbQlSDE0QTMgpxew/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDg2/Mzg4MDEwL3Bob3Rv/L2xhbWJvcmdoaW5p/LWF2ZW50YWRvci1s/cDcwMC00LXN1cGVy/Y2FyLW1vZGVsLWNh/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9TDExQkQ2LUhr/UTZUcGFaUkVhVjNf/akVTUThlWFAtZG5H/eWFQNWEzaXBEdz0" alt="img1"></img>
-          </div>
-          <div className="texts">
-            <h2>HURACÁN</h2>
-            <p>The upcoming Lamborghini Revuelto is the first in the Lambo range to be equipped with an electric drivetrain thanks to its plug-in hybrid system. It has a 6.5-litre unit with a peak torque output of 725Nm at 6,750rpm and 813bhp of maximum power at 9,250 rpm. </p>
-          </div>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<IndexPage />}/>
+      <Route path={'/login'} element={
+        <div>login page</div>
+      }/>
+      </Route>    
+    </Routes>
+   
+  
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,20 @@
+export default function Post(){
+    return(
+        <div className="entries">
+      <div className = "post">
+      <div className="image">
+        <img src="https://imgs.search.brave.com/-zhYr-__chwocwaLLbuMMDm87G6vbQlSDE0QTMgpxew/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDg2/Mzg4MDEwL3Bob3Rv/L2xhbWJvcmdoaW5p/LWF2ZW50YWRvci1s/cDcwMC00LXN1cGVy/Y2FyLW1vZGVsLWNh/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9TDExQkQ2LUhr/UTZUcGFaUkVhVjNf/akVTUThlWFAtZG5H/eWFQNWEzaXBEdz0" alt="img1"></img>
+        </div>
+        <div className="texts">
+          <h2>HURACÁN</h2>
+          <p className = "info">
+            <a className="author">reetika</a>
+            <time>2023-11-20 15:25</time>
+          </p>
+          <p className="summary">The upcoming Lamborghini Revuelto is the first in the Lambo range to be equipped with an electric drivetrain thanks to its plug-in hybrid system. It has a 6.5-litre unit with a peak torque output of 725Nm at 6,750rpm and 813bhp of maximum power at 9,250 rpm. </p>
+        </div>
+      </div>
+     
+    </div>
+    );
+}
